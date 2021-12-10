d922d6177c6fe7cd1ed38fd4ee0f3921c919d54c
Filip Luac <oliveripepa1207>
2021-12-10 12:35:11 +0100

    chore: added pdf

--- src/routes/dokumenti.svelte
+++ src/routes/dokumenti.svelte
@@ -1,84 +1,83 @@
 <svelte:head>
-    <title>Agrimatco | Dokumenti</title>
+  <title>Agrimatco | Dokumenti</title>
 </svelte:head>
 
 <style>
+  .stl-list {
+    columns: 3;
+  }
+  @media (max-width: 800px) {
     .stl-list {
-        columns: 3;
+      columns: 2;
     }
-    @media (max-width: 800px) {
-        .stl-list {
-            columns: 2;
-        }
-    }
+  }
 </style>
 
 <section class="section">
-    <div class="container">
-        <h1>Dokumenti</h1>
-        <div class="grid">
-            <figure class="col-6 col-s-12">
-                <figcaption><b>Letci</b></figcaption>
-                <ul>
-                    <li><a rel="prefetch" target="_blank" href="letci/alba-milagro.pdf">Alba Milagro (gnojiva)</a></li>
-                    <li><a rel="prefetch" target="_blank" href="letci/mulch-folije.pdf">Mulch folije</a></li>
-                    <li><a rel="prefetch" target="_blank" href="letci/lubenice.pdf">Lubenice</a></li>
-                    <li><a rel="prefetch" target="_blank" href="letci/paprike.pdf">Paprike</a></li>
-                    <li><a rel="prefetch" target="_blank" href="letci/salata.pdf">Salata</a></li>
-                    <li><a rel="prefetch" target="_blank" href="letci/kupusnjace.pdf">Kupusnjače</a></li>
-                    <li><a rel="prefetch" target="_blank" href="letci/novi-proizvodi.pdf">Novi proizvodi (2021/22)</a></li>
-                    <li><a rel="prefetch" target="_blank" href="letci/sjeme-povrca.pdf">Novi proizvodi (2021/22) - Sjeme povrća</a></li>
-                </ul>
-            </figure>
-            <figure class="col-6 col-s-12">
-                <figcaption><b>Katalozi</b></figcaption>
-                <ul>
-                    <li><a rel="prefetch" target="_blank" href="katalozi/2021-katalog.pdf">EKO Katalog - 2021</a></li>
-                    <li><a rel="prefetch" target="_blank" href="katalozi/2022-sjeme-povrca.pdf">Sjeme povrća - 2022</a></li>
-                </ul>
-            </figure>
-            <figure class="col-12">
-                <figcaption><b>Sigurnosno tehnički list (STL)</b></figcaption>
-                <ul class="stl-list">
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-agro-k.pdf"><u>Agro-K</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol390.pdf"><u>Agrofol 390</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol550k.pdf"><u>Agrofol 550K</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol650.pdf"><u>Agrofol 650</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-aminoton.pdf"><u>Aminoton</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-azocip.pdf"><u>Azocip</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-barrier.pdf"><u>Barrier</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-bioriviter.pdf"><u>Bio Riviter</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-bioplex.pdf"><u>Bioplex</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-boramide.pdf"><u>Boramide</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-calcibor.pdf"><u>Calcibor</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-calcium-mainstay.pdf"><u>Calcium Mainstay</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-delumbri-limax.pdf"><u>Delumbri Limax</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-dessicash200sl.pdf"><u>Dessicash 200SL</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-cash480sl.pdf"><u>Cash 480 SL</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-fasthrin10ec.pdf"><u>Fasthrin 10 EC</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-gallupsuper480.pdf"><u>Gallup Super 480</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-h-85.pdf"><u>H-85</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-inex.pdf"><u>Inex</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-kalexcu.pdf"><u>Kalex Cu</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-kalexzn.pdf"><u>Kalex Zn</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-kalex.pdf"><u>Kalex</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-b.pdf"><u>Kelatex B</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-ca-b.pdf"><u>Kelatex Ca+B</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-fe.pdf"><u>Kelatex Fe</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-zn.pdf"><u>Kelatex Zn</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-mainstay-magnesium.pdf"><u>Mainstay Magnesium</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-manfil80wpstlclp.pdf"><u>Manfil 80 WP</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-nico4sc.pdf"><u>Nico 4 SC</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-oligomix.pdf"><u>Oligomix</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-phmaster.pdf"><u>pH Master</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-prometca.pdf"><u>Promet Ca</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-prometzn.pdf"><u>Promet Zn</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-rootex.pdf"><u>Rootex</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-trazex.pdf"><u>Trazex</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-tropicel.pdf"><u>Tropicel</u></a></li>
-                    <li><a rel="prefetch" target="_blank" href="stl/stl-vigorizer.pdf"><u>Vigorizer</u></a></li>
-                </ul>
-            </figure>
-        </div>
+  <div class="container">
+    <h1>Dokumenti</h1>
+    <div class="grid">
+      <figure class="col-6 col-s-12">
+        <figcaption><b>Letci</b></figcaption>
+        <ul>
+          <li><a rel="prefetch" target="_blank" href="letci/alba-milagro.pdf">Alba Milagro (gnojiva)</a></li>
+          <li><a rel="prefetch" target="_blank" href="letci/mulch-folije.pdf">Mulch folije</a></li>
+          <li><a rel="prefetch" target="_blank" href="letci/lubenice.pdf">Lubenice</a></li>
+          <li><a rel="prefetch" target="_blank" href="letci/paprike.pdf">Paprike</a></li>
+          <li><a rel="prefetch" target="_blank" href="letci/salata.pdf">Salata</a></li>
+          <li><a rel="prefetch" target="_blank" href="letci/kupusnjace.pdf">Kupusnjače</a></li>
+          <li><a rel="prefetch" target="_blank" href="letci/sjeme-povrca.pdf">Novi proizvodi (2021/22) - Sjeme povrća</a></li>
+        </ul>
+      </figure>
+      <figure class="col-6 col-s-12">
+        <figcaption><b>Katalozi</b></figcaption>
+        <ul>
+          <li><a rel="prefetch" target="_blank" href="katalozi/2021-katalog.pdf">EKO Katalog - 2021</a></li>
+          <li><a rel="prefetch" target="_blank" href="katalozi/2022-sjeme-povrca.pdf">Sjeme povrća - 2022</a></li>
+        </ul>
+      </figure>
+      <figure class="col-12">
+        <figcaption><b>Sigurnosno tehnički list (STL)</b></figcaption>
+        <ul class="stl-list">
+          <li><a rel="prefetch" target="_blank" href="stl/stl-agro-k.pdf"><u>Agro-K</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol390.pdf"><u>Agrofol 390</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol550k.pdf"><u>Agrofol 550K</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol650.pdf"><u>Agrofol 650</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-aminoton.pdf"><u>Aminoton</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-azocip.pdf"><u>Azocip</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-barrier.pdf"><u>Barrier</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-bioriviter.pdf"><u>Bio Riviter</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-bioplex.pdf"><u>Bioplex</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-boramide.pdf"><u>Boramide</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-calcibor.pdf"><u>Calcibor</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-calcium-mainstay.pdf"><u>Calcium Mainstay</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-delumbri-limax.pdf"><u>Delumbri Limax</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-dessicash200sl.pdf"><u>Dessicash 200SL</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-cash480sl.pdf"><u>Cash 480 SL</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-fasthrin10ec.pdf"><u>Fasthrin 10 EC</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-gallupsuper480.pdf"><u>Gallup Super 480</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-h-85.pdf"><u>H-85</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-inex.pdf"><u>Inex</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kalexcu.pdf"><u>Kalex Cu</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kalexzn.pdf"><u>Kalex Zn</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kalex.pdf"><u>Kalex</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-b.pdf"><u>Kelatex B</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-ca-b.pdf"><u>Kelatex Ca+B</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-fe.pdf"><u>Kelatex Fe</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-zn.pdf"><u>Kelatex Zn</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-mainstay-magnesium.pdf"><u>Mainstay Magnesium</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-manfil80wpstlclp.pdf"><u>Manfil 80 WP</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-nico4sc.pdf"><u>Nico 4 SC</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-oligomix.pdf"><u>Oligomix</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-phmaster.pdf"><u>pH Master</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-prometca.pdf"><u>Promet Ca</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-prometzn.pdf"><u>Promet Zn</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-rootex.pdf"><u>Rootex</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-trazex.pdf"><u>Trazex</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-tropicel.pdf"><u>Tropicel</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-vigorizer.pdf"><u>Vigorizer</u></a></li>
+        </ul>
+      </figure>
     </div>
+  </div>
 </section>
