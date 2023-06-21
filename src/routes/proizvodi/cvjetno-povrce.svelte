7c979bd5a09b7b0493efc6cbc2b35f4f0dde65bd
floyyd6 <slobodantomic82@gmail.com>
2023-06-21 11:20:34 +0200

    chore: adding assets and content

--- src/routes/proizvodi/cvjetno-povrce.svelte
+++ src/routes/proizvodi/cvjetno-povrce.svelte
@@ -13,43 +13,43 @@
                     title: 'Green Magic F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/brokula/green-magic-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=30'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=28'
                 },
                 {
                     title: 'Naxos F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/brokula/naxos-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=30'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=28'
                 },
                 {
                     title: 'Parthenon F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/brokula/parthenon-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=31'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=28'
                 },
                 {
                     title: 'Ares F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/brokula/ares-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=31'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=28'
                 },
                 {
                     title: 'Triton F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/brokula/triton-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=31'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=29'
                 },
                 {
                     title: 'Thassos F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/brokula/thassos-f1.png',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=31'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=29'
                 },
                 {
                     title: 'Marathon F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/brokula/marathon-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=31'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=29'
                 }
             ]
         },
@@ -60,55 +60,55 @@
                     title: 'Alston F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/cvjetaca/alston-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=32'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=30'
                 },
                 {
                     title: 'Cashmere F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/cvjetaca/cashmere-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=32'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=30'
                 },
                 {
                     title: 'White step F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/cvjetaca/white-step-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=32'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=30'
                 },
                 {
                     title: 'Concept F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/cvjetaca/concept-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=32'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=30'
                 },
                 {
                     title: 'Merton F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/cvjetaca/merton-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=33'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=31'
                 },
                 {
                     title: 'Kastor F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/cvjetaca/kastor-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=33'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=31'
                 },
                 {
                     title: 'Whiton F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/cvjetaca/whiton-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=33'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=31'
                 },
                 {
                     title: 'Incline F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/cvjetaca/incline-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=34'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=32'
                 },
                 {
                     title: 'Jericho F1',
                     description: 'Dobavljač: Sakata',
                     image: 'sjeme-povrca/cvjetno-povrce/cvjetaca/jericho-f1.jpg',
-                    link: 'katalozi/2022-sjeme-povrca.pdf#page=34'
+                    link: 'katalozi/2022-sjeme-povrca.pdf#page=32'
                 },
             ]
         }
