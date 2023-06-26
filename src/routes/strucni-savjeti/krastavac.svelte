3c56bbfd4d7662fb4fb14cfe525a23808c2b3048
floyyd6 <slobodantomic82@gmail.com>
2023-06-26 10:47:34 +0200

    chore: adding assets,content and pages

---
+++ src/routes/strucni-savjeti/krastavac.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+    <title>Agrimatco | Stručni savjeti | Krastavac</title>
+</svelte:head>
+
+<img src="strucni-savjeti/krastavac/krastavac.png" alt="">
+
+<section class="section">
+    <div class="container">
+        <nav><small><a rel="prefetch" href="strucni-savjeti">Stručni savjeti</a> > Soja</small></nav>
+        <h1>Prihrana krastavca</h1>
+        <a href="katalozi/2020-prihrana.pdf#page=75">Pročitaj</a>
+    </div>
+</section>
---
+++ src/routes/strucni-savjeti/kukuruz-secerac.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+    <title>Agrimatco | Stručni savjeti | Kukuruz šecerac</title>
+</svelte:head>
+
+<img src="strucni-savjeti/kukuruz-secerac/secerac.png" alt="">
+
+<section class="section">
+    <div class="container">
+        <nav><small><a rel="prefetch" href="strucni-savjeti">Stručni savjeti</a> > Soja</small></nav>
+        <h1>Prihrana kukuruza šećerca</h1>
+        <a href="katalozi/2020-prihrana.pdf#page=79">Pročitaj</a>
+    </div>
+</section>
