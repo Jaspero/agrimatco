f1ccb430e3837e994911ec45f75e4c9f639b92d9
floyyd6 <slobodantomic82@gmail.com>
2023-06-30 12:38:34 +0200

    chore: style adjustments

--- src/routes/strucni-savjeti.svelte
+++ src/routes/strucni-savjeti.svelte
@@ -17,8 +17,8 @@
                     <li><a href="strucni-savjeti/orijentacione-granicne-ec-vrijednosti">Orijentacione granične EC vrijednosti</a></li>
                     <li><a href="strucni-savjeti/simptomi-deficita-elemenata-biljne-ishrane-na-liscu">Simptomi deficita elemenata biljne ishrane na lišću</a></li>
                     <li><a href="strucni-savjeti/znacaj-folijarne-prihrane">Značaj folijarne prihrane</a></li>
-                    <li><a href="katalozi/2020-sjeme-povrca.pdf#page=8">Kemijski sastav gnojiva</a></li>
-                    <li><a href="katalozi/2020-sjeme-povrca.pdf#page=81">Otpornost - rezistentnost na bolesti i štetnike</a></li>
+                    <li><a href="katalozi/2023-sjeme-povrca.pdf#page=8">Kemijski sastav gnojiva</a></li>
+                    <li><a href="katalozi/2023-sjeme-povrca.pdf#page=81">Otpornost - rezistentnost na bolesti i štetnike</a></li>
                 </ul>
             </figure>
             <figure class="col-6 col-s-12">
