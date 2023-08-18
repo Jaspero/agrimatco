ee774256e701f13adf985ed0cd2c1ef60d7757f1
AnteSuvala <ante@keeperewa.com>
2023-08-18 12:45:59 +0200

    chore: content update

--- src/routes/proizvodi/korjenasto-povrce.svelte
+++ src/routes/proizvodi/korjenasto-povrce.svelte
@@ -129,7 +129,7 @@
                 {
                     title: 'Soneto',
                     description: 'Dobavljač: Vilmorin',
-                    image: 'sjeme-povrca/korjenasto-povrce/mrkva/soneto-1.JPG',
+                    image: 'sjeme-povrca/korjenasto-povrce/mrkva/soneto-1.jpg',
                     link: 'katalozi/2023-sjeme-povrca.pdf#page=37'
                 }
             ]
