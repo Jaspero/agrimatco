79c002c33e76e9a64d49de493baea1bb9508423a
floyyd6 <slobodantomic82@gmail.com>
2023-08-25 14:24:22 +0200

    chore: content update

--- src/routes/dokumenti.svelte
+++ src/routes/dokumenti.svelte
@@ -39,40 +39,40 @@
       <figure class="col-12">
         <figcaption><b>Sigurnosno tehnički list (STL)</b></figcaption>
         <ul class="stl-list">
-          <li><a rel="prefetch" target="_blank" href="stl/STL-AGROFOL-390-1.pdf"><u>Agro-K</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol390.pdf"><u>Agrofol 390</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol550k.pdf"><u>Agrofol 550K</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol650.pdf"><u>Agrofol 650</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-aminoton.pdf"><u>Aminoton</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-Agro-K-1.pdf"><u>Agro-K</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol-390-1.pdf"><u>Agrofol 390</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol-550k-1.pdf"><u>Agrofol 550K</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol-650-1.pdf"><u>Agrofol 650</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-aminoton-1.pdf"><u>Aminoton</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-azocip.pdf"><u>Azocip</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-barrier.pdf"><u>Barrier</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-barrier-1.pdf"><u>Barrier</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-bioriviter.pdf"><u>Bio Riviter</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-bioplex.pdf"><u>Bioplex</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-boramide.pdf"><u>Boramide</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-calcibor.pdf"><u>Calcibor</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-calcium-mainstay.pdf"><u>Calcium Mainstay</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-delumbri-limax.pdf"><u>Delumbri Limax</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-bioplex-1.pdf"><u>Bioplex</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-boramide-1.pdf"><u>Boramide</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-calcibor-1.pdf"><u>Calcibor</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-calcium-mainstay-1.pdf"><u>Calcium Mainstay</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-delumbri-limax-1.pdf"><u>Delumbri Limax</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-dessicash200sl.pdf"><u>Dessicash 200SL</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-cash480sl.pdf"><u>Cash 480 SL</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-cash480sl-1.pdf"><u>Cash 480 SL</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-fasthrin10ec.pdf"><u>Fasthrin 10 EC</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-gallupsuper480.pdf"><u>Gallup Super 480</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-h-85.pdf"><u>H-85</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-inex.pdf"><u>Inex</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-gallupsuper480-1.pdf"><u>Gallup Super 480</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-h-85-1.pdf"><u>H-85</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-inex-1.pdf"><u>Inex</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-kalexcu.pdf"><u>Kalex Cu</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-kalexzn.pdf"><u>Kalex Zn</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-kalex.pdf"><u>Kalex</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-b.pdf"><u>Kelatex B</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-ca-b.pdf"><u>Kelatex Ca+B</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-b-1.pdf"><u>Kelatex B</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-ca-b-1.pdf"><u>Kelatex Ca+B</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-fe.pdf"><u>Kelatex Fe</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-zn.pdf"><u>Kelatex Zn</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-zn-1.pdf"><u>Kelatex Zn</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-mainstay-magnesium.pdf"><u>Mainstay Magnesium</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-manfil80wpstlclp.pdf"><u>Manfil 80 WP</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-nico4sc.pdf"><u>Nico 4 SC</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-oligomix.pdf"><u>Oligomix</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-phmaster.pdf"><u>pH Master</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-prometca.pdf"><u>Promet Ca</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-prometzn.pdf"><u>Promet Zn</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-rootex.pdf"><u>Rootex</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-nico4sc-1.pdf"><u>Nico 4 SC</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-oligomix-1.pdf"><u>Oligomix</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-phmaster-1.pdf"><u>pH Master</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-prometca-1.pdf"><u>Promet Ca</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-prometzn-1.pdf"><u>Promet Zn</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-rootex-1.pdf"><u>Rootex</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-trazex.pdf"><u>Trazex</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-tropicel.pdf"><u>Tropicel</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-vigorizer.pdf"><u>Vigorizer</u></a></li>
