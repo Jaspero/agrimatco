d29ed13ba8c2feec7e78e334178364c0a04d7562
floyyd6 <slobodantomic82@gmail.com>
2023-08-25 11:40:09 +0200

    chore: content update

--- src/routes/strucni-savjeti/bob.svelte
+++ src/routes/strucni-savjeti/bob.svelte
@@ -1,10 +1,11 @@
 <svelte:head>
     <title>Agrimatco | Stručni savjeti | Bob</title>
 </svelte:head>
-<div class="container">
-<img class="w-full" src="strucni-savjeti/bob/bob-1.jpeg" alt="">
-</div>
+
 <section class="section">
+    <div class="container">
+        <img class="w-full mt-10" src="strucni-savjeti/bob/bob-1.jpeg" alt="">
+    </div>
     <div class="container">
         <nav><small><a rel="prefetch" href="strucni-savjeti">Stručni savjeti</a> > Soja</small></nav>
         <h1>Folijarna prihrana Boba</h1>
