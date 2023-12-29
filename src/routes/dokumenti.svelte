05e158e64b45d6ab2ad77a2025f5acd2a2781476
floyyd6 <slobodantomic82@gmail.com>
2023-12-29 09:08:11 +0100

    chore: content update

--- src/routes/dokumenti.svelte
+++ src/routes/dokumenti.svelte
@@ -34,7 +34,7 @@
           <li><a rel="prefetch" target="_blank" href="katalozi/2021-katalog.pdf">EKO Katalog - 2021</a></li>
           <li><a rel="prefetch" target="_blank" href="katalozi/2023-sjeme-povrca.pdf">Sjeme povrća - 2023</a></li>
           <li><a rel="prefetch" target="_blank" href="katalozi/2023-gnojidba-prihrana-zastita.pdf">Gnojidba, prihrana, zaštita - 2023</a></li>
-          <li><a rel="prefetch" target="_blank" href="katalozi/sjeme-povrca-2024">Sjeme povrća - 2024</a></li>
+          <li><a rel="prefetch" target="_blank" href="katalozi/sjeme-povrca-2024.pdf">Sjeme povrća - 2024</a></li>
           <li><a rel="prefetch" target="_blank" href="katalozi/prihrana-bilja-2024.pdf">Gnojidba, prihrana, zaštita - 2024</a></li>
           <li><a rel="prefetch" target="_blank" href="katalozi/zastita-bilja-2024.pdf">Zaštita bilja - 2024</a></li>
         </ul>
