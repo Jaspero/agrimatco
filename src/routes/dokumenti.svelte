54d26650e3dc6bacb364b2631c626847e3521a24
lorka1 <lorena126os@gmail.com>
2024-11-20 10:21:47 +0100

    chore:added new pdfs

--- src/routes/dokumenti.svelte
+++ src/routes/dokumenti.svelte
@@ -50,21 +50,28 @@
           <li><a rel="prefetch" target="_blank" href="stl/stl-agrofol-n-300.pdf"><u>Agrofol N 300</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-amcolon-15-5-35.pdf"><u>Amcolon 15-5-35</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-amcolon-18-18-18.pdf"><u>Amcolon 18-18-18</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-amcopaste-8-11-18.pdf"><u>Amcopaste 8-11-28</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-amcopaste-12-30-12.pdf"><u>Amcopaste 12-30-6</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/amcopaste-8-11-28-2024.pdf"><u>Amcopaste 8-11-28</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/amcopaste-12-30-6-2024.pdf"><u>Amcopaste 12-30-6</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-amcopaste-13-13-13.pdf"><u>Amcopaste 13-13-13</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-amco-potato.pdf"><u>Amco-potato</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-aminoton-1.pdf"><u>Aminoton</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-apical-plus.pdf"><u>Apical Plus</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-asco-alga.pdf"><u>Asco Alga</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-asco-alga-2024.pdf"><u>Asco Alga</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-ascoleg.pdf"><u>Ascoleg</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-asco-start-tricho-b.pdf"><u>Asco Start Tricho B</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-azocip.pdf"><u>Azocip</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-backrow-max.pdf"><u>Backrow Max</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/backrow-max-2024.pdf"><u>Backrow Max</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-barrier-1.pdf"><u>Barrier</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/basfoliar-34-SL-2024.pdf"><u>Basfoliar Kelp O</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/basfoliar-34-SL-2024.pdf"><u>Basfoliar 34</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-biolife.pdf"><u>Biolife</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-biomax-frokum.pdf"><u>Biomax Frokum</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-biomax-karpum.pdf"><u>Biomax Karpum</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-biomax-magnum.pdf"><u>Biomax Magnum</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-biomax-solum.pdf"><u>Biomax Solum</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-bioplex-1.pdf"><u>Bioplex</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/blaukorn-12-12-17-2024.pdf"><u>Blaukorn 12-12-17</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-boramide-1.pdf"><u>Boramide</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-borelag.pdf"><u>Borealg</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-calcibor-1.pdf"><u>Calcibor</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-calcium-mainstay-1.pdf"><u>Calcium Mainstay</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-cash480sl-1.pdf"><u>Cash 480 SL</u></a></li>
@@ -78,65 +85,63 @@
           <li><a rel="prefetch" target="_blank" href="stl/stl-ekotron.pdf"><u>Ekotron</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-erune-40.pdf"><u>Erune 40</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-foliar-plus.pdf"><u>Foliar Plus</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-folisec.pdf"><u>Folisec</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-folisec-2024.pdf"><u>Folisec</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-gallupsuper480-1.pdf"><u>Gallup Super 480</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-granu-potasse.pdf"><u>GranuPotasse</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-granupotase-2024.pdf"><u>GranuPotasse</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-h-85-1.pdf"><u>H-85</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-herfomyc.pdf"><u>Herfomyc</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-herofsec.pdf"><u>Herfosec</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-herfomyc-2024.pdf"><u>Herfomyc</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-herfosec-2024.pdf"><u>Herfosec</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-humo-grow.pdf"><u>Humo Grow</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-hurler.pdf"><u>Hurler</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-hydroponica-calcium-nitrate.pdf"><u>Hydroponica Calcium Nitrate</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-hydroponica-calmag.pdf"><u>Hydroponica Calmag</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/hydroponica-calnit-2024.pdf"><u>Hydroponica Calcium Nitrate</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/hydroponica-calmag-2024.pdf"><u>Hydroponica Calmag</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-inex-1.pdf"><u>Inex</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-kalexcu.pdf"><u>Kalex Cu</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-kalexzn.pdf"><u>Kalex Zn</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-kalex.pdf"><u>Kalex</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-ketarel.pdf"><u>Kantarel</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/kalex-evo-2024.pdf"><u>Kalex Evo</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-kantarel-2024.pdf"><u>Kantarel</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-b-1.pdf"><u>Kelatex B</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-ca-b-1.pdf"><u>Kelatex Ca+B</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-fe.pdf"><u>Kelatex Fe</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-kelatex-zn-1.pdf"><u>Kelatex Zn</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-keymag-ruby-10-00-40-2MgO.pdf"><u>Keymag Ruby 10-00-40+2MgO</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-mainstay-magnesium.pdf"><u>Mainstay Magnesium</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/keymag-10-0-40-2024.pdf"><u>Keymag Ruby 10-00-40+2MgO</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/kwality-n-2024.pdf"><u>Kwality N</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-maisotrione-100-CS.pdf"><u>Maisotrione 100 CS</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-manfil80wpstlclp.pdf"><u>Manfil 80 WP</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-molbor.pdf"><u>Molbor</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-multiphose-mg.pdf"><u>Multiphos Mg</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/multiphos-mg-2024.pdf"><u>Multiphos Mg</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-multi-n.pdf"><u>Multi N</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-nemadate.pdf"><u>Nemadate</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-nemater.pdf"><u>Nemater</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-nexus-s.pdf"><u>Nexur S</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-nico4sc-1.pdf"><u>Nico 4 SC</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-novalon-10-10-40+TE.pdf"><u>Novalon 10-10-40+TE</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-novalon-12-48-06+TE.pdf"><u>Novalon 12-48-06+TE</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-novalon-15-05-35.pdf"><u>Novalon 15-05-35</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-novalon-19-06-20+2MgO.pdf"><u>Novalon 19-06-20+2MgO</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-novalon-20-20-20+TE.pdf"><u>Novalon 20-20-20+TE</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-novalon-30-10-10+2MgO+TE.pdf"><u>Novalon 30-10-10+2MgO+TE</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-novalon-6-12-36.pdf"><u>Novalon 6-12-36+3MgO+TE</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-novalon-00-05-35.pdf"><u>Novalon 00-05-35+4,5MgO+TE</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-novatec-40.pdf"><u>Novatec 40</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-novatec-pro.pdf"><u>Novatec pro</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/nemater-2024.pdf"><u>Nemater</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/nexur-s-2024.PDF"><u>Nexur S</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-nico-4-sc.pdf"><u>Nico 4 SC</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novalon-10-10-40-2024.pdf"><u>Novalon 10-10-40+TE</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novalon-12-48-6-2024.pdf"><u>Novalon 12-48-06+TE</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novalon-15-5-35-2024.pdf"><u>Novalon 15-05-35</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novalon-19-6-20-2024.pdf"><u>Novalon 19-06-20+2MgO</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novalon-20-20-20-2024.pdf"><u>Novalon 20-20-20+TE</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novalon-30-10-10-2024.pdf"><u>Novalon 30-10-10+2MgO+TE</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novalon-6-12-36-2024.pdf"><u>Novalon 6-12-36+3MgO+TE</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novalon-0-5-35-2024.pdf"><u>Novalon 00-05-35+4,5MgO+TE</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novatec-40-2024.pdf"><u>Novatec 40</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novatec-classic-12-8-16-2024.pdf"><u>NovaTec Classic 12-8-16</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/novatec-triplo-15-9-15-2024.pdf"><u>NovaTec Triplo 15-9-15</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/nova-tec-pro-2024.pdf"><u>Novatec pro</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/nutrigold-20-20-20-2024.pdf"><u>Nutrigold</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-oligomix-1.pdf"><u>Oligomix</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-phmaster-1.pdf"><u>pH Master</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-phospoactive.pdf"><u>Phospoactive</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-plaza.pdf"><u>Plaza</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/praktis-2024.pdf"><u>Praktis</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-prometca-1.pdf"><u>Promet Ca</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-prometzn-1.pdf"><u>Promet Zn</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-rivitter-HFA.pdf"><u>Rivitter HFA</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/riviter-hfa-2024.pdf"><u>Rivitter HFA</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-rootex-1.pdf"><u>Rootex</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-sangral.pdf"><u>Sangral® Potassium Nitrate Prilled</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/sangral-potassium-nitrate-prilled-2024.pdf"><u>Sangral® Potassium Nitrate Prilled</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-solupotasse.pdf"><u>SoluPotasse</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-soptech.pdf"><u>Soptech</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/solupotase-2024.pdf"><u>Soptech</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-souverain-do.pdf"><u>Souverain DO</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-staygreen.pdf"><u>Staygreen</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/sticker-2024.pdf"><u>Sticker</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-tebu-super-250-EW.pdf"><u>Tebu super 250 EW</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-tercol.pdf"><u>Tercol</u></a></li>
+          <li><a rel="prefetch" target="_blank" href="stl/stl-tercol-2024.pdf"><u>Tercol</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-trazex-1.pdf"><u>Trazex</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-tropicel-1.pdf"><u>Tropicel</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-TSL-29.pdf"><u>TSL 29</u></a></li>
-          <li><a rel="prefetch" target="_blank" href="stl/stl-vigorizer.pdf"><u>Vigorizer</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-WOPRO-TANA-NPK-3-4-3.pdf"><u>WOPRO TANA NPK 3-4-3</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-WOPRO-TANA-NPK-4-1-6.pdf"><u>WOPRO TANA NPK 4-1-6</u></a></li>
           <li><a rel="prefetch" target="_blank" href="stl/stl-WOPRO-TANA-NPK-9-0-0.pdf"><u>WOPRO TANA NPK 9-0-0</u></a></li>
