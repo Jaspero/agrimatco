ed9c606a776d0959fee340621ffe5bd078054ca0
bridget75 <93909566+bridget75@users.noreply.github.com>
2025-01-21 11:10:08 +0100

    fix: changing pdf document and removing the old ones

--- src/routes/dokumenti.svelte
+++ src/routes/dokumenti.svelte
@@ -27,8 +27,7 @@
       <figure class="col-6 col-s-12">
         <figcaption><b>Katalozi</b></figcaption>
         <ul>
-          <li><a rel="prefetch" target="_blank" href="katalozi/2023-gnojidba-prihrana-zastita.pdf">Gnojidba, prihrana, zaštita - 2023</a></li>
-          <li><a rel="prefetch" target="_blank" href="katalozi/prihrana-bilja-2024.pdf">Gnojidba, prihrana, zaštita - 2024</a></li>
+          <li><a rel="prefetch" target="_blank" href="katalozi/Gnojidba, prihrana, zaštita - 2025.pdf">Gnojidba, prihrana, zaštita - 2025</a></li>
           <li><a rel="prefetch" target="_blank" href="katalozi/katalog-zastita-bilja-2025.pdf">Zaštita bilja - 2025</a></li>
           <li><a rel="prefetch" target="_blank" href="katalozi/sjeme-povrca-2025.pdf">Sjeme povrća - 2025</a></li>
         </ul>
