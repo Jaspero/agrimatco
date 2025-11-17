1241ec45c20f13c725dcd7e564ac9329c5d66d47
floyyd6 <slobodantomic82@gmail.com>
2025-11-17 12:38:06 +0100

    chore: updating to new svelte version

---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+    let year = new Date().getFullYear();
+</script>
+
+<style>
+    .footer {
+        border-top: 1px solid var(--border);
+        padding: var(--space-medium);
+        font-size: var(--font-small);
+        text-align: center;
+    }
+</style>
+
+<footer class="footer">
+    <p><a href="." rel="prefetch"><img src="agrimatco.svg" alt="Agrimatco" width="120"></a></p>
+    <div>Pratite našu <a href="https://web.facebook.com/Agrimatco-Croatia-356855041012103" target="_blank" rel="noopener">Facebook</a> stranicu.</div>
+    <div>Copyright © {year} Agrimatco d.o.o.</div>
+    <div>Sva prava pridržana.</div>
+    <div>Made by <a href="https://jaspero.co/" target="_blank" rel="noopener">Jaspero</a>.</div>
+    <div><a href="/politika-privatnosti" rel="noopener">Politika privatnosti</a>.</div>
+</footer>
